73721e042ba81a36b6cd2346de56de03e9865592
Justin Valentin <Justinvalentine88@gmail.com>
2017-03-22 08:25:40 -0400

    adventure mode

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,39 +1,24 @@
-#have a random number for user to guess
-
-# puts"" for a number between 1 and 100
-#gets "" a prompt for the number
-
-#if users guess is less than the program number it should
-#  tell the user that their guess was low and let guess again
-
-  # ig users guess is greater than the program number it
-  #  should tell you that you were high and let guess again
-  #
-  #  if the guess is correct the program should tell the user
-  #    that they win and then quit
-  #
-  #    after 5 incorrect guesses the program should say
-  #    you lose
-
-
-
 def ask_for_number
   print "Would you like to play a game? if yes enter a number if no, go away "
   answer = gets.chomp.to_i
   answer
+
 end
+numbers = []
+
 
 def guessing
   guess_count = 1
-  # random_number = rand(101) + 1
-  random_number = 3
+  random_number = rand(101) + 1
   guess = ask_for_number
   while guess_count < 5
+
 
       if guess < random_number
         puts " your number is less than my number, try again
         or don't it's fine either way. "
         guess = ask_for_number
+        numbers << answer
       elsif guess > random_number
         puts " your number is greater than my number, are you even
         trying? "
@@ -51,6 +36,14 @@ def guessing
       else
         puts "YOU LOSE, Forever. "
       end
+
 end
 
-guessing
+
+
+if numbers.include?(@answer)
+  puts "don't be a jackass"
+end
+
+
+  guessing
